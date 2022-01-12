51129885a51dd0171b6aa87b5a7235ea0a12289b
rand0mdev <77558763+rand0mdev@users.noreply.github.com>
2022-01-12 01:51:32 +0100

    chore(code): clean up the contract direstory to prepare branching

--- contracts/interfaces/ICauldron.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
-
-interface ICauldron {
-    function userCollateralShare(address account) external view returns (uint256);
-}
--- contracts/interfaces/IVotingEligibility.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
-
-interface IVotingEligibility {
-    function isEligible(address _account) external view returns (bool);
-}
