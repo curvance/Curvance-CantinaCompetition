11e1a23b10f80a84149fa14ce05128dfa3a46d81
rand0mdev <77558763+rand0mdev@users.noreply.github.com>
2022-08-09 00:38:38 +0200

    Proposed airdrop implementation (#23)
    * Full rewrite of CVE Merkle Tree Airdrop
    
    * Added canClaimAirdrop for easier frontend implementation and cleaned up documentation. Pauses contract on deployment.
    
    * Fix 1 second difference between claimAirdrop and canClaimAirdrop
    
    * chore: disable coverage since we do not use hardhat
    
    Co-authored-by: 0xMai <102006339+ZeroXMai@users.noreply.github.com>

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -1,83 +1,145 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// Allows anyone to claim a token if they exist in a merkle root.
-interface IMerkleDistributor {
-    // Returns the merkle root of the merkle tree containing account balances available to claim.
-    function merkleRoot() external view returns (bytes32);
-
-    // Returns true if the index has been marked claimed.
-    function isClaimed(uint256 index) external view returns (bool);
-
-    // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
-    function claim(
-        uint256 index,
-        address account,
-        uint256 amount,
-        bytes32[] calldata merkleProof
-    ) external;
-
-    // This event is triggered whenever a call to #claim succeeds.
-    event Claimed(uint256 index, address account, uint256 amount);
-}
-
-interface IMerkleAirdropFactory {
-    function owner() external returns (address);
-}
-
-contract MerkleAirdrop is IMerkleDistributor, Initializable {
+contract CVEAirdrop {
     using SafeERC20 for IERC20;
-    using Address for address;
 
-    address public factory;
-    address public token;
+    event CVEAirdropClaimed(address indexed claimer, uint256 amount);
+    event RemainingCVEWithdrawn(uint256 amount);
+    event OwnerUpdated(address indexed user, address indexed newOwner);
 
-    uint256 public startClaimTimestamp;
-    uint256 public endClaimTimestamp;
-    bytes32 public override merkleRoot;
+    IERC20  public immutable cveToken;
+    uint256 public immutable maximumClaimAmount;
+    uint256 public immutable endClaimTimestamp;
 
-    // This is a packed array of booleans.
-    mapping(uint256 => uint256) private claimedBitMap;
+    bytes32 public           airdropMerkleRoot;
+    address public           owner;
+    bool    public           isPaused = true;
+    uint256 private          locked = 1;
+
+    mapping(address => bool) public airdropClaimed;
+
+     /**
+     * @notice Constructor
+     * @param _cve address of the CVE token
+     * @param _endTimestamp end timestamp for airdrop claiming
+     * @param _root Airdrop merkle root for claim validation
+     * @param _maximumClaimAmount maximum amount to claim per address
+     */
+    constructor (
+        address _cve, 
+        uint256 _endTimestamp,
+        uint256 _maximumClaimAmount,  
+        bytes32 _root
+    ) {
+        cveToken = IERC20(_cve);
+        endClaimTimestamp = _endTimestamp;
+        maximumClaimAmount = _maximumClaimAmount; 
+        airdropMerkleRoot = _root;
+
+        owner = _msgSender();
+        emit OwnerUpdated(address(0), owner);
+    }
 
     modifier onlyOwner() {
-        require(msg.sender == IMerkleAirdropFactory(factory).owner(), "!auth");
+        require(_msgSender() == owner, "Not Owner");
         _;
     }
 
-    constructor() {}
+    modifier notPaused() {
+        require(!isPaused, "Airdrop Paused");
+        _;
+    }
 
-    receive() external payable {}
+    modifier nonReentrant() {
+        require(locked == 1, "Reentry Attempt");
+        locked = 2;
+        _;
+        locked = 1;
+    }
 
-    /**
-     * @dev initialize contract variables
-     * @param _token airdrop token
-     * @param _startTime time to start claiming airdrop
-     * @param _merkleRoot merkle root hash of merkleroot implementation
-     */
-    function init(
-        address _token,
-        uint256 _startTime,
-        uint256 _endTime,
-        bytes32 _merkleRoot
-    ) external initializer {
-        require(_token != address(0), "!valid token");
-        require(block.timestamp >= _startTime && block.timestamp < _endTime, "!valid time");
-        factory = msg.sender;
-        token = _token;
-        startClaimTimestamp = _startTime;
-        endClaimTimestamp = _endTime;
-        merkleRoot = _merkleRoot;
+    function _msgSender() internal view returns (address) {
+        return msg.sender;
     }
 
     /**
-     * @dev rescue any token sent by mistake
+     * @notice Claim CVE tokens for airdrop
+     * @param _amount Requested CVE amount to claim for the airdrop
+     * @param _proof Bytes32 array containing the merkle proof
+     */
+    function claimAirdrop(uint256 _amount, bytes32[] calldata _proof) external notPaused nonReentrant {
+        // Verify that the airdrop Merkle Root has been set
+        require(airdropMerkleRoot != bytes32(0), "claimAirdrop: Airdrop Merkle Root not set");
+
+        // Verify CVE amount request is not above the maximum claim amount
+        require(_amount <= maximumClaimAmount, "claimAirdrop: Amount too high");
+
+        // Verify Claim window has not passed
+        require(block.timestamp < endClaimTimestamp, "claimAirdrop: Too late to claim");
+
+        // Verify the user has not claimed their airdrop already
+        require(!airdropClaimed[_msgSender()], "claimAirdrop: Already claimed");
+
+        // Compute the merkle leaf and verify the merkle proof
+        require(verifyProof(_proof, airdropMerkleRoot, keccak256(abi.encodePacked(_msgSender(), _amount))), "claimAirdrop: Invalid proof provided");
+
+        // Document that airdrop has been claimed
+        airdropClaimed[_msgSender()] = true;
+
+        // Transfer CVE tokens
+        cveToken.safeTransfer(_msgSender(), _amount);
+
+        emit CVEAirdropClaimed(_msgSender(), _amount);
+    }
+
+    /**
+     * @notice Gas efficient merkle proof verification implementation to validate CVE token claim
+     * @param _proof Requested CVE amount to claim for the airdrop
+     * @param _root Merkle root to check computed hash against
+     * @param _leaf Merkle leaf containing hashed inputs to compare proof element against 
+     */
+    function verifyProof(bytes32[] memory _proof, bytes32 _root, bytes32 _leaf) internal pure returns (bool) {
+            bytes32 computedHash = _leaf;
+            uint256 iterations = _proof.length;
+            bytes32 proofElement;
+
+            for (uint256 i; i < iterations; ) {
+                proofElement = _proof[i++];
+
+                if (computedHash <= proofElement) {
+                    computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+                } else {
+                    computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+                }
+
+            }
+            return computedHash == _root;
+    }
+
+    /**
+     * @notice Check whether a user has CVE tokens to claim
+     * @param _address address of the user to check
+     * @param _amount amount to claim
+     * @param _proof array containing the merkle proof
+     */
+    function canClaimAirdrop(
+        address _address,
+        uint256 _amount,
+        bytes32[] calldata _proof
+    ) external view returns (bool) {
+        if (!airdropClaimed[_address]){
+            if (block.timestamp < endClaimTimestamp) {
+                // Compute the leaf and verify the merkle proof
+                return verifyProof(_proof, airdropMerkleRoot, keccak256(abi.encodePacked(_address, _amount)));
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @notice rescue any tokens sent by mistake
      * @param _token token to rescue
      * @param _recipient address to receive token
      */
@@ -86,75 +148,52 @@ contract MerkleAirdrop is IMerkleDistributor, Initializable {
         address _recipient,
         uint256 _amount
     ) external onlyOwner {
-        require(_recipient != address(0), "!valid recipient");
+        require(_recipient != address(0), "rescueToken: Invalid recipient address");
         if (_token == address(0)) {
-            require(address(this).balance >= _amount, "!amount");
+            require(address(this).balance >= _amount, "rescueToken: Insufficient balance");
             (bool success, ) = payable(_recipient).call{ value: _amount }("");
-            require(success, "!successful");
+            require(success, "rescueToken: !successful");
         } else {
-            require(IERC20(_token).balanceOf(address(this)) >= _amount, "!amount");
+            require(IERC20(_token).balanceOf(address(this)) >= _amount, "rescueToken: Insufficient balance");
             SafeERC20.safeTransfer(IERC20(_token), _recipient, _amount);
         }
     }
 
     /**
-     * @dev Set time to end claim.
-     * @param _endTime new timestamp.
+     * @notice Withdraws unclaimed airdrop tokens to contract Owner after airdrop claim period has ended
      */
-    function setClaimEndTimestamp(uint256 _endTime) external onlyOwner {
-        require(_endTime > block.timestamp && _endTime > startClaimTimestamp, "!valid");
-        endClaimTimestamp = _endTime;
+    function withdrawRemainingAirdropTokens() external onlyOwner {
+        require(block.timestamp > endClaimTimestamp, "withdrawRemainingAirdropTokens: Too early");
+        uint256 tokensToWithdraw = cveToken.balanceOf(address(this));
+        cveToken.safeTransfer(_msgSender(), tokensToWithdraw);
+
+        emit RemainingCVEWithdrawn(tokensToWithdraw);
     }
 
     /**
-     * @notice Check if airdrop claimed.
-     * @param index index to check.
-     * @return bool
+     * @notice Set airdropMerkleRoot for airdrop validation
+     * @param _root new merkle root
      */
-    function isClaimed(uint256 index) public view override returns (bool) {
-        uint256 claimedWordIndex = index / 256;
-        uint256 claimedBitIndex = index % 256;
-        uint256 claimedWord = claimedBitMap[claimedWordIndex];
-        uint256 mask = (1 << claimedBitIndex);
-        return claimedWord & mask == mask;
+    function setMerkleRoot(bytes32 _root) external onlyOwner {
+        require(_root != bytes32(0), "setMerkleRoot: Invalid Parameter");
+        airdropMerkleRoot = _root;
     }
 
     /**
-     * @notice Set airdrop claimed.
-     * @param index index to set claimed.
+     * @notice Set contract Owner
+     * @param _newOwner new contract Owner
      */
-    function _setClaimed(uint256 index) private {
-        uint256 claimedWordIndex = index / 256;
-        uint256 claimedBitIndex = index % 256;
-        claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
+    function setOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+        emit OwnerUpdated(_msgSender(), _newOwner);
     }
 
     /**
-     * @notice Claim airdrop.
-     * @param index index at which to claim.
-     * @param account claiming account.
-     * @param amount claiming amount
-     * @param merkleProof sequence of merkle proof.
+     * @notice Set isPaused state
+     * @param _state new pause state
      */
-    function claim(
-        uint256 index,
-        address account,
-        uint256 amount,
-        bytes32[] calldata merkleProof
-    ) external override {
-        require(block.timestamp >= startClaimTimestamp && block.timestamp < endClaimTimestamp, "!valid time");
-        require(!isClaimed(index), "already claimed.");
-
-        // Verify the merkle proof.
-        bytes32 node = keccak256(abi.encodePacked(index, account, amount));
-        require(MerkleProof.verify(merkleProof, merkleRoot, node), "!valid proof");
-
-        // Mark it claimed and send the token.
-        _setClaimed(index);
-
-        // transfer to account
-        IERC20(token).safeTransfer(account, amount);
-
-        emit Claimed(index, account, amount);
+    function setPauseState(bool _state) external onlyOwner {
+        isPaused = _state;
     }
+
 }
