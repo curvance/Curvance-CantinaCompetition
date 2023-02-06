11362dbd37740b1395f40ee2a3489e6d2d0ef8f7
zeuscurvance <zeus@curvance.com>
2023-02-06 12:34:13 -0500

    implement epoch based gauge pool controller

--- contracts/gauges/GaugeController.sol
+++ contracts/gauges/GaugeController.sol
@@ -7,34 +7,82 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./GaugeErrors.sol";
 
 contract GaugeController is Ownable {
+    // structs
+    struct Epoch {
+        uint256 rewardPerSec;
+        uint256 totalAllocPoint;
+        mapping(address => uint256) poolAllocPoint; // token => alloc point
+    }
+
+    uint256 public constant EPOCH_WINDOW = 4 weeks;
+
     // storage
     address public cve;
-    uint256 public rewardPerSec;
-    uint256 public totalAllocPoint;
-    mapping(address => uint256) public poolAllocPoint; // token => alloc point
+
+    uint256 public startTime;
+    mapping(uint256 => Epoch) internal epochInfo;
 
     constructor(address _cve) Ownable() {
         cve = _cve;
     }
 
+    function start() external onlyOwner {
+        if (startTime != 0) {
+            revert GaugeErrors.AlreadyStarted();
+        }
+
+        startTime = 0;
+    }
+
+    function currentEpoch() public view returns (uint256) {
+        return (block.timestamp - startTime) / EPOCH_WINDOW;
+    }
+
+    function epochOfTimestamp(uint256 timestamp) public pure returns (uint256) {
+        return timestamp / EPOCH_WINDOW;
+    }
+
+    function epochStartTime(uint256 epoch) public view returns (uint256) {
+        return startTime + epoch * EPOCH_WINDOW;
+    }
+
+    function epochEndTime(uint256 epoch) public view returns (uint256) {
+        return startTime + (epoch + 1) * EPOCH_WINDOW;
+    }
+
     function isGaugeEnabled(address token) public view returns (bool) {
-        return poolAllocPoint[token] > 0;
+        return isGaugeEnabled(currentEpoch(), token);
     }
 
-    function updateRewardPerSec(uint256 _rewardPerSec) external onlyOwner {
-        rewardPerSec = _rewardPerSec;
+    function isGaugeEnabled(uint256 epoch, address token) public view returns (bool) {
+        return epochInfo[epoch].poolAllocPoint[token] > 0;
     }
 
-    function updateEmissionRates(address[] memory tokens, uint256[] memory allocPoints) external onlyOwner {
+    function setRewardPerSecOfNextEpoch(uint256 epoch, uint256 _rewardPerSec) external onlyOwner {
+        if (epoch != currentEpoch() + 1) {
+            revert GaugeErrors.InvalidEpoch();
+        }
+
+        epochInfo[epoch].rewardPerSec = _rewardPerSec;
+    }
+
+    function updateEmissionRates(
+        uint256 epoch,
+        address[] memory tokens,
+        uint256[] memory allocPoints
+    ) external onlyOwner {
+        if (epoch != currentEpoch() + 1) {
+            revert GaugeErrors.InvalidEpoch();
+        }
+
         if (tokens.length != allocPoints.length) {
             revert GaugeErrors.InvalidLength();
         }
 
-        massUpdatePools(tokens);
-
+        Epoch storage info = epochInfo[epoch];
         for (uint256 i = 0; i < tokens.length; ++i) {
-            totalAllocPoint = totalAllocPoint + allocPoints[i] - poolAllocPoint[tokens[i]];
-            poolAllocPoint[tokens[i]] = allocPoints[i];
+            info.totalAllocPoint = info.totalAllocPoint + allocPoints[i] - info.poolAllocPoint[tokens[i]];
+            info.poolAllocPoint[tokens[i]] = allocPoints[i];
         }
     }
 
