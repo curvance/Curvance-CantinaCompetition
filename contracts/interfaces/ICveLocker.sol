aacf51a6ca0388c85c5babc5a48c0376ee3ea662
0xMai <102006339+ZeroXMai@users.noreply.github.com>
2023-03-18 16:42:47 -0400

    Mai new CVE token and CVE Locker interfaces

---
+++ contracts/interfaces/ICveLocker.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface ICveLocker {
+    function getBalance() external view returns (uint256);
+
+    function withdrawFor(address _user, uint256 amount) external;
+    function withdrawOnShutdown(uint256 amount) external;
+    function stake() external;
+    function distribute() external;
+    function getRewards(address _user) external view returns (uint256);
+    function claimRewards(address _user) external;
+    function stake(uint256 amount) external;
+}
---
+++ contracts/token/CVE.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.12;
+import "../layerzero/OFT.sol";
+
+error InvalidGas();
+
+abstract contract cve is OFT {
+
+    constructor(string memory _name, 
+                string memory _symbol, 
+                address _lzEndpoint) OFT(_name, _symbol, _lzEndpoint) {
+                    _mint(msg.sender, 420000069);
+                }
+
+    
+    function sendFrom(address _from, uint16 _dstChainId, bytes calldata _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParams) public payable override (OFTCore, IOFTCore){
+
+        // Estimate the transaction fees
+        (uint256 messageFee, ) = estimateSendFee(_dstChainId, _toAddress, _amount, false, _adapterParams);
+
+        // Check if the provided gas is sufficient
+        if(msg.value < messageFee) revert InvalidGas();
+
+        super.sendFrom(_from, 
+              _dstChainId, 
+              _toAddress, 
+              _amount, 
+              _refundAddress, 
+              _zroPaymentAddress, 
+              _adapterParams);
+
+    }
+
+    function createAdapterParams(uint16 version, uint256 gasForTraverse) public pure returns (bytes memory) {
+        return abi.encodePacked(version, gasForTraverse);
+    }
+
+}
