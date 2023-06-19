8cda7e3fa5d953f1a51d65e261470dc364cbfd8d
sather1009 <Sather@Curvance.com>
2023-06-19 23:47:27 +0900

    remove duplicated interfaces

--- contracts/interfaces/ICurvePool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.17;
-
-interface ICurvePool {
-    function coins(uint256 i) external view returns (address);
-
-    function get_virtual_price() external view returns (uint256);
-}
