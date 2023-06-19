926683540d740a8635b737094c1084863ec8e928
sather1009 <Sather@Curvance.com>
2023-06-20 00:18:29 +0900

    add TestBase

---
+++ tests/utils/TestBase.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {Test} from "forge-std/Test.sol";
+
+contract TestBase is Test {
+    uint256 internal constant _ONE = 1e18;
+}
