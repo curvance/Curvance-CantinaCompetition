6b4766e0efb0f70b8f8a56e7139b4852324ac5ff
sather1009 <Sather@Curvance.com>
2023-06-19 22:09:09 +0900

    update Timelock with openzeppelin TimelockController@v4.9.0

--- contracts/compound/Timelock.sol
+++ contracts/compound/Timelock.sol
@@ -6,12 +6,11 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 contract Timelock is TimelockController {
     uint256 public constant MINIMUM_DELAY = 2 days;
 
-    constructor(address admin) TimelockController(MINIMUM_DELAY, new address[](0), new address[](0)) {
+    constructor(address admin) TimelockController(MINIMUM_DELAY, new address[](0), new address[](0), admin) {
         // remove admin role from deployer
         _revokeRole(TIMELOCK_ADMIN_ROLE, _msgSender());
 
         // grant admin/proposer/executor role to admin
-        _setupRole(TIMELOCK_ADMIN_ROLE, admin);
         _setupRole(PROPOSER_ROLE, admin);
         _setupRole(EXECUTOR_ROLE, admin);
     }
