dc2b087b5d0539e8534163c70483776a67dadb82
sather1009 <Sather@Curvance.com>
2023-06-20 12:15:57 +0900

    fix typo with TestZapperOneInch contract name

--- tests/zapper/TestZapperOneInch.t.sol
+++ tests/zapper/TestZapperOneInch.t.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 import "tests/utils/TestBase.sol";
 
-contract testZapperOneInch is TestBase {
+contract TestZapperOneInch is TestBase {
     address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
