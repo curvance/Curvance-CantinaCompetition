892af493a7b7c28a9cdb1240511fd0216335f193
0xMai <102006339+ZeroXMai@users.noreply.github.com>
2023-07-28 00:57:59 -0400

    cUSDC -> mUSDC in PriceRouter tests

--- tests/oracles/PriceRouterV2/functions/AddMTokenSupport.t.sol
+++ tests/oracles/PriceRouterV2/functions/AddMTokenSupport.t.sol
@@ -8,16 +8,16 @@ contract AddMTokenSupportTest is TestBasePriceRouter {
         vm.prank(address(1));
 
         vm.expectRevert("centralRegistry: UNAUTHORIZED");
-        priceRouter.addMTokenSupport(address(cUSDC));
+        priceRouter.addMTokenSupport(address(mUSDC));
     }
 
     function test_addMTokenSupport_fail_whenMTokenIsAlreadyConfigured()
         public
     {
-        priceRouter.addMTokenSupport(address(cUSDC));
+        priceRouter.addMTokenSupport(address(mUSDC));
 
         vm.expectRevert("PriceRouter: MToken already configured");
-        priceRouter.addMTokenSupport(address(cUSDC));
+        priceRouter.addMTokenSupport(address(mUSDC));
     }
 
     function test_addMTokenSupport_fail_whenMTokenIsInvalid() public {
@@ -27,15 +27,15 @@ contract AddMTokenSupportTest is TestBasePriceRouter {
 
     function test_addMTokenSupport_success() public {
         (bool isMToken, address underlying) = priceRouter.MTokenAssets(
-            address(cUSDC)
+            address(mUSDC)
         );
 
         assertFalse(isMToken);
         assertEq(underlying, address(0));
 
-        priceRouter.addMTokenSupport(address(cUSDC));
+        priceRouter.addMTokenSupport(address(mUSDC));
 
-        (isMToken, underlying) = priceRouter.MTokenAssets(address(cUSDC));
+        (isMToken, underlying) = priceRouter.MTokenAssets(address(mUSDC));
 
         assertTrue(isMToken);
         assertEq(underlying, _USDC_ADDRESS);
--- tests/oracles/PriceRouterV2/functions/RemoveMTokenSupport.t.sol
+++ tests/oracles/PriceRouterV2/functions/RemoveMTokenSupport.t.sol
@@ -8,27 +8,27 @@ contract RemoveMTokenSupportTest is TestBasePriceRouter {
         vm.prank(address(1));
 
         vm.expectRevert("centralRegistry: UNAUTHORIZED");
-        priceRouter.removeMTokenSupport(address(cUSDC));
+        priceRouter.removeMTokenSupport(address(mUSDC));
     }
 
     function test_removeMTokenSupport_fail_whenMTokenIsNotConfigured() public {
         vm.expectRevert("PriceRouter: MToken is not configured");
-        priceRouter.removeMTokenSupport(address(cUSDC));
+        priceRouter.removeMTokenSupport(address(mUSDC));
     }
 
     function test_removeMTokenSupport_success() public {
-        priceRouter.addMTokenSupport(address(cUSDC));
+        priceRouter.addMTokenSupport(address(mUSDC));
 
         (bool isMToken, address underlying) = priceRouter.MTokenAssets(
-            address(cUSDC)
+            address(mUSDC)
         );
 
         assertTrue(isMToken);
         assertEq(underlying, _USDC_ADDRESS);
 
-        priceRouter.removeMTokenSupport(address(cUSDC));
+        priceRouter.removeMTokenSupport(address(mUSDC));
 
-        (isMToken, underlying) = priceRouter.MTokenAssets(address(cUSDC));
+        (isMToken, underlying) = priceRouter.MTokenAssets(address(mUSDC));
 
         assertFalse(isMToken);
         assertEq(underlying, address(0));
