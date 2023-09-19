3efc8c48a2adb0c23da547ae788c1a9843145e36
zeuscurvance <zeus@curvance.com>
2023-09-19 08:09:44 -0400

    start tokens testing

---
+++ tests/market/collateral/integrations/TestTokens.t.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { IMToken } from "contracts/interfaces/market/IMToken.sol";
+import { IUniswapV2Router } from "contracts/interfaces/external/uniswap/IUniswapV2Router.sol";
+import { SwapperLib } from "contracts/libraries/SwapperLib.sol";
+
+import "tests/market/TestBaseMarket.sol";
+
+contract User {}
+
+contract TestTokens is TestBaseMarket {
+    address internal constant _UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    address public owner;
+
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    function setUp() public override {
+        super.setUp();
+
+        owner = address(this);
+
+        // start epoch
+        gaugePool.start(address(lendtroller));
+
+        // deploy dDAI
+        {
+            _deployDDAI();
+            // support market
+            _prepareDAI(owner, 200000e18);
+            dai.approve(address(dDAI), 200000e18);
+            lendtroller.listMarketToken(address(dDAI));
+            // add MToken support on price router
+            priceRouter.addMTokenSupport(address(dDAI));
+            address[] memory markets = new address[](1);
+            markets[0] = address(dDAI);
+            vm.prank(user1);
+            lendtroller.enterMarkets(markets);
+            vm.prank(user2);
+            lendtroller.enterMarkets(markets);
+        }
+
+        // deploy CBALRETH
+        {
+            // deploy aura position vault
+            _deployCBALRETH();
+
+            // support market
+            _prepareBALRETH(owner, 1 ether);
+            balRETH.approve(address(cBALRETH), 1 ether);
+            lendtroller.listMarketToken(address(cBALRETH));
+            // add MToken support on price router
+            priceRouter.addMTokenSupport(address(cBALRETH));
+            // set collateral factor
+            lendtroller.updateCollateralToken(
+                IMToken(address(cBALRETH)),
+                200,
+                0,
+                5000
+            );
+            address[] memory markets = new address[](1);
+            markets[0] = address(cBALRETH);
+            vm.prank(user1);
+            lendtroller.enterMarkets(markets);
+            vm.prank(user2);
+            lendtroller.enterMarkets(markets);
+        }
+
+        // provide enough liquidity
+        provideEnoughLiquidityForLeverage();
+
+        centralRegistry.addSwapper(_UNISWAP_V2_ROUTER);
+    }
+
+    function provideEnoughLiquidityForLeverage() internal {
+        address liquidityProvider = address(new User());
+        _prepareDAI(liquidityProvider, 200000e18);
+        _prepareBALRETH(liquidityProvider, 10 ether);
+        // mint dDAI
+        vm.startPrank(liquidityProvider);
+        dai.approve(address(dDAI), 200000 ether);
+        dDAI.mint(200000 ether);
+        // mint cBALETH
+        balRETH.approve(address(cBALRETH), 10 ether);
+        cBALRETH.mint(10 ether);
+        vm.stopPrank();
+    }
+
+    function testInitialize() public {
+        assertEq(cBALRETH.isCToken(), true);
+        assertEq(dDAI.isDToken(), true);
+    }
+
+    function testCTokenMintRedeem() public {
+        _prepareBALRETH(user1, 2 ether);
+
+        // try mint()
+        vm.startPrank(user1);
+        balRETH.approve(address(cBALRETH), 1 ether);
+        cBALRETH.mint(1 ether);
+        vm.stopPrank();
+        assertEq(cBALRETH.balanceOf(user1), 1 ether);
+
+        // try mintFor()
+        vm.startPrank(user1);
+        balRETH.approve(address(cBALRETH), 1 ether);
+        cBALRETH.mintFor(1 ether, user2);
+        vm.stopPrank();
+        assertEq(cBALRETH.balanceOf(user1), 1 ether);
+        assertEq(cBALRETH.balanceOf(user2), 1 ether);
+
+        // try redeem()
+        vm.startPrank(user1);
+        cBALRETH.redeem(1 ether);
+        vm.stopPrank();
+        assertEq(cBALRETH.balanceOf(user1), 0);
+
+        // try redeemUnderlying()
+        vm.startPrank(user2);
+        cBALRETH.redeemUnderlying(1 ether);
+        vm.stopPrank();
+        assertEq(cBALRETH.balanceOf(user2), 0);
+    }
+
+    function testDTokenMintRedeem() public {
+        _prepareDAI(user1, 2 ether);
+
+        // try mint()
+        vm.startPrank(user1);
+        dai.approve(address(dDAI), 1 ether);
+        dDAI.mint(1 ether);
+        vm.stopPrank();
+        assertEq(dDAI.balanceOf(user1), 1 ether);
+
+        // try mintFor()
+        vm.startPrank(user1);
+        dai.approve(address(dDAI), 1 ether);
+        dDAI.mintFor(1 ether, user2);
+        vm.stopPrank();
+        assertEq(dDAI.balanceOf(user1), 1 ether);
+        assertEq(dDAI.balanceOf(user2), 1 ether);
+
+        // try redeem()
+        vm.startPrank(user1);
+        dDAI.redeem(1 ether);
+        vm.stopPrank();
+        assertEq(dDAI.balanceOf(user1), 0);
+
+        // try redeemUnderlying()
+        vm.startPrank(user2);
+        dDAI.redeemUnderlying(1 ether);
+        vm.stopPrank();
+        assertEq(dDAI.balanceOf(user2), 0);
+    }
+
+    function testDTokenBorrowRepay() public {
+        // testAccountSnapshot
+    }
+
+    function testCTokenRedeemOnBorrow() public {
+        // testAccountSnapshot
+    }
+
+    function testDTokenRedeemOnBorrow() public {
+        // testAccountSnapshot
+    }
+
+    function testCTokenTransferOnBorrow() public {
+        // testAccountSnapshot
+    }
+
+    function testDTokenTransferOnBorrow() public {
+        // testAccountSnapshot
+    }
+
+    function testLiquidation() public {
+        // testAccountSnapshot
+    }
+
+    function testExchangeRate() public {
+        // testAccountSnapshot
+    }
+}
