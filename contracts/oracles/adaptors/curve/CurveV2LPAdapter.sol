6ca15e4094bb6139d36e69166da022ad26ccc4ce
zeuscurvance <zeus@curvance.com>
2023-10-05 21:55:30 -0400

    implement current adapter workflow

--- contracts/oracles/adaptors/curve/CurveV2LPAdapter.sol
+++ contracts/oracles/adaptors/curve/CurveV2LPAdapter.sol
@@ -5,6 +5,7 @@ import { BaseOracleAdaptor } from "contracts/oracles/adaptors/BaseOracleAdaptor.
 
 import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
 import { PriceReturnData } from "contracts/interfaces/IOracleAdaptor.sol";
+import { IPriceRouter } from "contracts/interfaces/IPriceRouter.sol";
 import { ICurvePool } from "contracts/interfaces/external/curve/ICurvePool.sol";
 
 contract CurveV2LPAdapter is BaseOracleAdaptor {
@@ -12,18 +13,34 @@ contract CurveV2LPAdapter is BaseOracleAdaptor {
     error CurveV2LPAdapter__DidNotConverge();
 
     // Technically I think curve pools will at most have 3 assets
-    // or atleast the majority have 2-3 so we could replace this struct with 5 addresses.
+    // or atleast the majority have 2-3
+    // so we could replace this struct with 5 addresses.
     struct AdaptorData {
         address[] coins;
         address pool;
     }
 
+    /// CONSTANTS ///
+
+    /// @notice Error code for bad source.
+    uint256 public constant BAD_SOURCE = 2;
+
+    uint256 private constant GAMMA0 = 28000000000000;
+    uint256 private constant A0 = 2 * 3 ** 3 * 10000;
+    uint256 private constant DISCOUNT0 = 1087460000000000;
+
+    /// STORAGE ///
+
     /// @notice Curve Pool Adaptor Storage
     mapping(address => AdaptorData) public adaptorData;
+
+    /// CONSTRUCTOR ///
 
     constructor(
         ICentralRegistry centralRegistry_
     ) BaseOracleAdaptor(centralRegistry_) {}
+
+    /// EXTERNAL FUNCTIONS ///
 
     /// @notice Add a Balancer Stable Pool Bpt as an asset.
     /// @dev Should be called before `PriceRotuer:addAssetPriceFeed` is called.
@@ -56,8 +73,7 @@ contract CurveV2LPAdapter is BaseOracleAdaptor {
 
         // Save values in Adaptor storage.
         adaptorData[asset].coins = coins;
-        adapterData[asset].pool = pool;
-        adaptorData[asset] = data;
+        adaptorData[asset].pool = pool;
         isSupportedAsset[asset] = true;
     }
 
@@ -79,12 +95,55 @@ contract CurveV2LPAdapter is BaseOracleAdaptor {
             .notifyAssetPriceFeedRemoval(asset);
     }
 
-    uint256 private constant GAMMA0 = 28000000000000;
-    uint256 private constant A0 = 2 * 3 ** 3 * 10000;
-    uint256 private constant DISCOUNT0 = 1087460000000000;
+    function getPrice(
+        address asset,
+        bool inUSD,
+        bool getLower
+    ) external view override returns (PriceReturnData memory pData) {
+        pData.inUSD = inUSD;
 
-    // x has 36 decimals
-    // result has 18 decimals.
+        AdaptorData memory adapter = adaptorData[asset];
+        IPriceRouter priceRouter = IPriceRouter(centralRegistry.priceRouter());
+        (uint256 price0, uint256 errorCode) = priceRouter.getPrice(
+            adapter.coins[0],
+            inUSD,
+            getLower
+        );
+        if (errorCode > 0) {
+            pData.hadError = true;
+            if (errorCode == BAD_SOURCE) return pData;
+        }
+
+        ICurvePool pool = ICurvePool(adapter.pool);
+        if (adapter.coins.length == 2) {
+            uint256 poolLpPrice = pool.lp_price();
+            pData.price = uint240((poolLpPrice * price0) / 1e18);
+        } else if (adapter.coins.length == 3) {
+            uint256 virtualPrice = pool.get_virtual_price();
+            uint256 t1Price = pool.price_oracle(0);
+            uint256 t2Price = pool.price_oracle(1);
+            uint256 maxPrice = (3 *
+                virtualPrice *
+                _cubicRoot(t1Price * t2Price)) / 1e18;
+
+            {
+                uint256 g = (pool.gamma() * 1e18) / GAMMA0;
+                uint256 a = (pool.A() * 1e18) / A0;
+                uint256 coefficient = (g ** 2 / 1e18) * a;
+                uint256 discount = coefficient > 1e34 ? coefficient : 1e34;
+                discount = (_cubicRoot(discount) * DISCOUNT0) / 1e18;
+
+                maxPrice -= (maxPrice * discount) / 1e18;
+            }
+
+            pData.price = uint240((maxPrice * price0) / 1e18);
+        }
+    }
+
+    /// INTERNAL FUNCTIONS ///
+
+    /// @dev x has 36 decimals
+    /// result has 18 decimals.
     function _cubicRoot(uint256 x) internal pure returns (uint256) {
         uint256 D = x / 1e18;
         for (uint8 i; i < 256; ++i) {
@@ -98,68 +157,5 @@ contract CurveV2LPAdapter is BaseOracleAdaptor {
             if (diff <= 1 || diff * 10 ** 18 < D) return D;
         }
         revert CurveV2LPAdapter__DidNotConverge();
-    }
-
-    function getPrice(
-        address asset,
-        bool inUSD,
-        bool getLower
-    ) external view override returns (PriceReturnData memory pData) {}
-
-    /// Inspired by https:// etherscan.io/address/0xE8b2989276E2Ca8FDEA2268E3551b2b4B2418950#code
-    /// @notice Get the price of a CurveV2 derivative in terms of Base.
-    function getPriceInBase(
-        uint64 sourceId
-    )
-        external
-        view
-        override
-        onlyPriceOps
-        returns (uint256 upper, uint256 lower, uint8 errorCode)
-    {
-        AdaptorData memory stor = getAdaptorData[sourceId];
-
-        ICurvePool pool = ICurvePool(stor.pool);
-
-        // Check that virtual price is within bounds.
-        uint256 virtualPrice = pool.get_virtual_price();
-        // VirtualPriceBound memory vpBound = getVirtualPriceBound[address(asset)];
-        // uint256 upper = uint256(vpBound.datum).mulDivDown(vpBound.posDelta, 1e8);
-        // upper = upper.changeDecimals(8, 18);
-        // uint256 lower = uint256(vpBound.datum).mulDivDown(vpBound.negDelta, 1e8);
-        // lower = lower.changeDecimals(8, 18);
-        // _checkBounds(lower, upper, virtualPrice);
-        // TODO if VP check fails, then return an error code.
-
-        uint256 token0Upper;
-        uint256 token0Lower;
-        (token0Upper, token0Lower, errorCode) = priceOps.getPriceInBase(
-            stor.coins[0]
-        );
-        if (errorCode == BAD_SOURCE) return (0, 0, BAD_SOURCE);
-
-        if (stor.coins.length == 2) {
-            uint256 poolLpPrice = pool.lp_price();
-            upper = poolLpPrice.mulDivDown(token0Upper, 1e18);
-            lower = poolLpPrice.mulDivDown(token0Lower, 1e18);
-        } else if (stor.coins.length == 3) {
-            uint256 t1Price = pool.price_oracle(0); // btc to usdt 18 dec
-            uint256 t2Price = pool.price_oracle(1); // eth to usdt 18 dec
-
-            uint256 maxPrice = (3 *
-                virtualPrice *
-                _cubicRoot(t1Price * t2Price)) / 1e18;
-            {
-                uint256 g = pool.gamma().mulDivDown(1e18, GAMMA0);
-                uint256 a = pool.A().mulDivDown(1e18, A0);
-                uint256 coefficient = (g ** 2 / 1e18) * a;
-                uint256 discount = coefficient > 1e34 ? coefficient : 1e34;
-                discount = _cubicRoot(discount).mulDivDown(DISCOUNT0, 1e18);
-
-                maxPrice -= maxPrice.mulDivDown(discount, 1e18);
-            }
-            upper = maxPrice.mulDivDown(token0Upper, 1e18);
-            lower = maxPrice.mulDivDown(token0Lower, 1e18);
-        }
     }
 }
