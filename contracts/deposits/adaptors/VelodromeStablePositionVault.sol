12661608898e61f392245bca6250bd1ab6660a3b
zeuscurvance <zeus@curvance.com>
2023-10-23 11:11:35 -0400

    fix M-33

--- contracts/deposits/adaptors/VelodromeStablePositionVault.sol
+++ contracts/deposits/adaptors/VelodromeStablePositionVault.sol
@@ -81,6 +81,12 @@ contract VelodromeStablePositionVault is BasePositionVault {
         // Query underlying token data from the pool
         strategyData.token0 = IVeloPool(_asset).token0();
         strategyData.token1 = IVeloPool(_asset).token1();
+        // make sure token0 is VELO if one of underlying tokens is VELO
+        // so that it can be used properly in harvest function.
+        if (strategyData.token1 == address(rewardToken)) {
+            strategyData.token1 = strategyData.token0;
+            strategyData.token0 = address(rewardToken);
+        }
         strategyData.decimalsA = 10 ** ERC20(strategyData.token0).decimals();
         strategyData.decimalsB = 10 ** ERC20(strategyData.token1).decimals();
 
@@ -160,6 +166,7 @@ contract VelodromeStablePositionVault is BasePositionVault {
                 }
             }
 
+            // token0 is VELO of one of underlying tokens is VELO
             // swap token0 to LP Token underlying tokens
             uint256 totalAmountA = ERC20(sd.token0).balanceOf(address(this));
 
