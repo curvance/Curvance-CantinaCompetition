ab4649b28cf51bd3f64d2b0e2566f95eceba5536
sather1009 <Sather@Curvance.com>
2023-10-26 11:19:37 +0900

    add tests cases for sequencer check

--- tests/oracles/PriceRouter/functions/GetPrice.t.sol
+++ tests/oracles/PriceRouter/functions/GetPrice.t.sol
@@ -1,13 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import { IChainlink } from "contracts/interfaces/external/chainlink/IChainlink.sol";
 import { TestBasePriceRouter } from "../TestBasePriceRouter.sol";
+import { IChainlink } from "contracts/interfaces/external/chainlink/IChainlink.sol";
+import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+import { ChainlinkAdaptor } from "contracts/oracles/adaptors/chainlink/ChainlinkAdaptor.sol";
 import { PriceRouter } from "contracts/oracles/PriceRouter.sol";
+import { MockDataFeed } from "contracts/mocks/MockDataFeed.sol";
 
 contract GetPriceTest is TestBasePriceRouter {
+    MockDataFeed public sequencer;
+
+    function setUp() public override {
+        super.setUp();
+
+        sequencer = new MockDataFeed(address(0));
+
+        chainlinkAdaptor = new ChainlinkAdaptor(
+            ICentralRegistry(address(centralRegistry)),
+            address(sequencer)
+        );
+
+        chainlinkAdaptor.addAsset(_USDC_ADDRESS, _CHAINLINK_USDC_USD, true);
+        chainlinkAdaptor.addAsset(_USDC_ADDRESS, _CHAINLINK_USDC_ETH, false);
+    }
+
     function test_getPrice_fail_whenNoFeedsAvailable() public {
         vm.expectRevert(PriceRouter.PriceRouter__NotSupported.selector);
+        priceRouter.getPrice(_USDC_ADDRESS, true, true);
+    }
+
+    function test_getPrice_fail_whenSequencerIsDown() public {
+        _addSinglePriceFeed();
+        sequencer.setMockAnswer(1);
+
+        vm.expectRevert(
+            ChainlinkAdaptor.ChainlinkAdaptor__SequencerIsDown.selector
+        );
+        priceRouter.getPrice(_USDC_ADDRESS, true, true);
+    }
+
+    function test_getPrice_fail_whenGracePeriodNotOver() public {
+        _addSinglePriceFeed();
+        sequencer.setMockStartedAt(block.timestamp);
+
+        vm.expectRevert(
+            ChainlinkAdaptor.ChainlinkAdaptor__GracePeriodNotOver.selector
+        );
         priceRouter.getPrice(_USDC_ADDRESS, true, true);
     }
 
