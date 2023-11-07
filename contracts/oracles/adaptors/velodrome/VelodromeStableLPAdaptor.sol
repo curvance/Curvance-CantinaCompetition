f4d795fad8313a04f951bfb0096fb3eddef92fde
sather1009 <Sather@Curvance.com>
2023-11-08 00:04:39 +0900

    update velodrome oracle adaptors with base adaptors

--- contracts/oracles/adaptors/velodrome/VelodromeStableLPAdaptor.sol
+++ contracts/oracles/adaptors/velodrome/VelodromeStableLPAdaptor.sol
@@ -1,45 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { BaseOracleAdaptor } from "contracts/oracles/adaptors/BaseOracleAdaptor.sol";
+import { BaseStableLPAdaptor } from "contracts/oracles/adaptors/utils/BaseStableLPAdaptor.sol";
 
-import { IERC20 } from "contracts/interfaces/IERC20.sol";
 import { PriceReturnData } from "contracts/interfaces/IOracleAdaptor.sol";
 import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
-import { IPriceRouter } from "contracts/interfaces/IPriceRouter.sol";
-import { IVeloPair } from "contracts/interfaces/external/velodrome/IVeloPair.sol";
-import { Math } from "contracts/libraries/Math.sol";
+import { IVeloPool } from "contracts/interfaces/external/velodrome/IVeloPool.sol";
 
-contract VelodromeStableLPAdaptor is BaseOracleAdaptor {
-    /// TYPES ///
-
-    /// @notice Adaptor storage
-    /// @param token0 token0 address
-    /// @param decimals0 token0 decimals
-    /// @param token1 token1 address
-    /// @param decimals1 token1 decimals
-    struct AdaptorData {
-        address token0;
-        uint8 decimals0;
-        address token1;
-        uint8 decimals1;
-    }
-
-    /// STORAGE ///
-
-    /// @notice Balancer Stable Pool Adaptor Storage
-    mapping(address => AdaptorData) public adaptorData;
-
+contract VelodromeStableLPAdaptor is BaseStableLPAdaptor {
     /// ERRORS ///
 
     error VelodromeStableLPAdaptor__AssetIsNotSupported();
     error VelodromeStableLPAdaptor__AssetIsAlreadyAdded();
+    error VelodromeStableLPAdaptor__AssetIsNotStableLP();
 
     /// CONSTRUCTOR ///
 
     constructor(
         ICentralRegistry centralRegistry_
-    ) BaseOracleAdaptor(centralRegistry_) {}
+    ) BaseStableLPAdaptor(centralRegistry_) {}
 
     /// EXTERNAL FUNCTIONS ///
 
@@ -54,74 +33,28 @@ contract VelodromeStableLPAdaptor is BaseOracleAdaptor {
         address asset,
         bool inUSD,
         bool getLower
-    ) external view override returns (PriceReturnData memory pData) {
+    ) external view override returns (PriceReturnData memory) {
         if (!isSupportedAsset[asset]) {
             revert VelodromeStableLPAdaptor__AssetIsNotSupported();
         }
 
-        // Read Adaptor storage and grab pool tokens
-        AdaptorData memory data = adaptorData[asset];
-        IVeloPair pool = IVeloPair(asset);
-
-        // LP total supply
-        uint256 totalSupply = pool.totalSupply();
-        // LP reserves
-        (uint256 reserve0, uint256 reserve1, ) = pool.getReserves();
-        // convert to 18 decimals
-        if (data.decimals0 != 18) {
-            reserve0 = (reserve0 * 1e18) / (10 ** data.decimals0);
-        }
-        if (data.decimals1 != 18) {
-            reserve1 = (reserve1 * 1e18) / (10 ** data.decimals1);
-        }
-
-        uint256 price0;
-        uint256 price1;
-        uint256 errorCode;
-        IPriceRouter priceRouter = IPriceRouter(centralRegistry.priceRouter());
-        (price0, errorCode) = priceRouter.getPrice(
-            data.token0,
-            inUSD,
-            getLower
-        );
-        if (errorCode > 0) {
-            pData.hadError = true;
-            return pData;
-        }
-        (price1, errorCode) = priceRouter.getPrice(
-            data.token1,
-            inUSD,
-            getLower
-        );
-        if (errorCode > 0) {
-            pData.hadError = true;
-            return pData;
-        }
-
-        pData.inUSD = inUSD;
-        pData.price = uint240(
-            _getFairPrice(reserve0, reserve1, price0, price1, totalSupply)
-        );
+        return _getPrice(asset, inUSD, getLower);
     }
 
     /// @notice Add a Balancer Stable Pool Bpt as an asset.
     /// @dev Should be called before `PriceRotuer:addAssetPriceFeed` is called.
     /// @param asset The address of the bpt to add
-    function addAsset(address asset) external onlyElevatedPermissions {
+    function addAsset(
+        address asset
+    ) external override onlyElevatedPermissions {
         if (isSupportedAsset[asset]) {
             revert VelodromeStableLPAdaptor__AssetIsAlreadyAdded();
         }
+        if (!IVeloPool(asset).stable()) {
+            revert VelodromeStableLPAdaptor__AssetIsNotStableLP();
+        }
 
-        IVeloPair pool = IVeloPair(asset);
-        AdaptorData memory data;
-        data.token0 = pool.token0();
-        data.token1 = pool.token1();
-        data.decimals0 = IERC20(data.token0).decimals();
-        data.decimals1 = IERC20(data.token1).decimals();
-
-        // Save values in Adaptor storage.
-        adaptorData[asset] = data;
-        isSupportedAsset[asset] = true;
+        _addAsset(asset);
     }
 
     /// @notice Removes a supported asset from the adaptor.
@@ -131,34 +64,6 @@ contract VelodromeStableLPAdaptor is BaseOracleAdaptor {
             revert VelodromeStableLPAdaptor__AssetIsNotSupported();
         }
 
-        // Notify the adaptor to stop supporting the asset
-        delete isSupportedAsset[asset];
-        // Wipe config mapping entries for a gas refund
-        delete adaptorData[asset];
-
-        // Notify the price router that we are going to stop supporting the asset
-        IPriceRouter(centralRegistry.priceRouter()).notifyFeedRemoval(asset);
-    }
-
-    /// INTERNAL FUNCTIONS ///
-
-    function _getFairPrice(
-        uint256 reserve0,
-        uint256 reserve1,
-        uint256 price0,
-        uint256 price1,
-        uint256 totalSupply
-    ) internal pure returns (uint256 fairReserve) {
-        // constant product = x^3 * y + x * y^3
-        uint256 sqrtReserve = Math.sqrt(
-            Math.sqrt(reserve0 * reserve1) *
-                Math.sqrt(reserve0 * reserve0 + reserve1 * reserve1)
-        );
-        uint256 ratio = ((1e18) * price0) / price1;
-        uint256 sqrtPrice = Math.sqrt(
-            Math.sqrt((1e18) * ratio) * Math.sqrt(1e36 + ratio * ratio)
-        );
-        return
-            ((((1e18) * sqrtReserve) / sqrtPrice) * price0 * 2) / totalSupply;
+        _removeAsset(asset);
     }
 }
