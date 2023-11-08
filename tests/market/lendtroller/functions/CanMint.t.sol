5a369f84884550f41bedba2625662cec3c8e5a6f
Gabriel Jaldon <gjaldon85@gmail.com>
2023-11-08 10:03:07 +0800

    CanMint tests

---
+++ tests/market/lendtroller/functions/CanMint.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import { TestBaseLendtroller } from "../TestBaseLendtroller.sol";
+import { Lendtroller } from "contracts/market/lendtroller/Lendtroller.sol";
+import { IMToken } from "contracts/interfaces/market/IMToken.sol";
+
+contract CanMintTest is TestBaseLendtroller {
+    function test_canMint_fail_whenMintPaused() public {
+        lendtroller.listMarketToken(address(dUSDC));
+
+        lendtroller.setMintPaused(IMToken(address(dUSDC)), true);
+        vm.expectRevert(Lendtroller.Lendtroller__Paused.selector);
+        lendtroller.canMint(address(dUSDC));
+    }
+
+    function test_canMint_fail_whenTokenNotListed() public {
+        vm.expectRevert(Lendtroller.Lendtroller__TokenNotListed.selector);
+        lendtroller.canMint(address(dUSDC));
+    }
+
+    function test_canMint_success() public {
+        lendtroller.listMarketToken(address(dUSDC));
+        lendtroller.canMint(address(dUSDC));
+    }
+}
