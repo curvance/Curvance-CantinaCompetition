3c5088ba27ae77f465ba622cd3d95d9def1d8aa9
zeuscurvance <zeus@curvance.com>
2023-11-27 12:09:26 -0500

    add veCve deployer

---
+++ scripts/deployers/VeCveDeployer.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+
+import { VeCVE } from "contracts/token/VeCVE.sol";
+import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+
+contract VeCveDeployer is Script {
+    address veCve;
+
+    function deployVeCve(
+        address centralRegistry,
+        uint256 clPointMultiplier
+    ) internal {
+        require(centralRegistry != address(0), "Set the centralRegistry!");
+
+        veCve = address(
+            new VeCVE(ICentralRegistry(centralRegistry), clPointMultiplier)
+        );
+
+        console.log("veCve: ", veCve);
+    }
+}
