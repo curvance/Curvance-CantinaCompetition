a9498717818fb120ed809d5ae5b3d149ea0b95c6
zeuscurvance <zeus@curvance.com>
2023-11-27 20:44:55 -0500

    add gauge pool deployer

---
+++ scripts/deployers/GuagePoolDeployer.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+
+import { GaugePool } from "contracts/gauge/GaugePool.sol";
+import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
+
+contract GuagePoolDeployer is Script {
+    address gaugePool;
+
+    function deployGaugePool(address centralRegistry) internal {
+        require(centralRegistry != address(0), "Set the centralRegistry!");
+
+        gaugePool = address(new GaugePool(ICentralRegistry(centralRegistry)));
+
+        console.log("gaugePool: ", gaugePool);
+    }
+}
