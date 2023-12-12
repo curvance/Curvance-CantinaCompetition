4550eaeec9009305e119a52d1ee9d48c09edd13c
sather1009 <Sather@Curvance.com>
2023-12-12 19:21:24 +0900

    update CVE deployment scripts

--- script/DeployCurvance.s.sol
+++ script/DeployCurvance.s.sol
@@ -66,6 +66,7 @@ contract DeployCurvance is
 
         _deployCve(
             centralRegistry,
+            _readConfigAddress(".cve.tokenBridgeRelayer"),
             _readConfigAddress(".cve.teamAddress"),
             _readConfigUint256(".cve.daoTreasuryAllocation"),
             _readConfigUint256(".cve.callOptionAllocation"),
