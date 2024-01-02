937e5e5282dd615ec5318a7e5efe2ee36f160f64
0xMai <chris@curvance.com>
2024-01-02 00:47:08 -0500

    finalize maker interfaces

---
+++ contracts/interfaces/external/maker/IMaker.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface DaiJoinLike {
+    function vat() external returns (VatLike);
+
+    function dai() external returns (GemLike);
+
+    function join(address, uint256) external payable;
+
+    function exit(address, uint256) external;
+}
+
+interface GemLike {
+    function approve(address, uint256) external;
+
+    function balanceOf(address) external view returns (uint256);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+}
+
+interface IVatLike {
+    function dai(address) external view returns (uint256);
+
+    function hope(address) external;
+}
--- contracts/interfaces/external/maker/IVatLike.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface VatLike {
-    function dai(address) external view returns (uint256);
-
-    function hope(address) external;
-}
