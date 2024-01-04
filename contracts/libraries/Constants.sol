9dd00293948a2ce56dca4ef04fb78c600b6a9de9
0xMai <chris@curvance.com>
2024-01-04 13:49:42 -0500

    fix constants library

--- contracts/libraries/Constants.sol
+++ contracts/libraries/Constants.sol
@@ -8,10 +8,10 @@ uint256 constant WAD = 1e18;
 uint256 constant DENOMINATOR = 1e4;
 
 /// @dev Return value indicating no price error.
-uint256 public constant NO_ERROR = 0;
+uint256 constant NO_ERROR = 0;
 
 /// @dev Return value indicating price divergence or 1 missing price.
-uint256 public constant CAUTION = 1;
+uint256 constant CAUTION = 1;
 
 /// @dev Return value indicating no price returned at all.
-uint256 public constant BAD_SOURCE = 2;
+uint256 constant BAD_SOURCE = 2;
