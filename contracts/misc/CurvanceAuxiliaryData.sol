ce7dcb7b9a1c59f7bb5664c40ce7d22052fa1dbe
0xMai <chris@curvance.com>
2024-01-07 22:08:23 -0500

    add remaining skeleton of auxiliary data functions

--- contracts/misc/CurvanceAuxiliaryData.sol
+++ contracts/misc/CurvanceAuxiliaryData.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { ERC165Checker } from "contracts/libraries/ERC165Checker.sol";
 import { CTokenCompounding } from "contracts/market/collateral/CTokenCompounding.sol";
 import { DToken } from "contracts/market/collateral/DToken.sol";
 
+import { ERC165Checker } from "contracts/libraries/ERC165Checker.sol";
+import { WAD, DENOMINATOR } from "contracts/libraries/Constants.sol";
+
+import { ILendtroller } from "contracts/interfaces/market/ILendtroller.sol";
+import { IMToken } from "contracts/interfaces/market/IMToken.sol";
 import { ICentralRegistry } from "contracts/interfaces/ICentralRegistry.sol";
 import { IGaugePool } from "contracts/interfaces/IGaugePool.sol";
-import { ILendtroller } from "contracts/interfaces/market/ILendtroller.sol";
 import { IFeeAccumulator } from "contracts/interfaces/IFeeAccumulator.sol";
 import { IPriceRouter } from "contracts/interfaces/IPriceRouter.sol";
 import { ICVELocker } from "contracts/interfaces/ICVELocker.sol";
-import { WAD, DENOMINATOR } from "contracts/libraries/Constants.sol";
+
 
 /// @notice An auxiliary contract for querying nuanced data 
 ///         inside the Curvance ecosystem.
@@ -45,6 +48,52 @@ contract CurvanceAuxiliaryData {
         centralRegistry = centralRegistry_; 
     }
 
+    /// EXTERNAL FUNCTIONS ///
+
+    function getTotalTVL() external view returns (uint256) {
+
+    }
+
+    function getTotalBorrows() external view returns (uint256) {
+
+    }
+
+    function getMarketTVL(address lendingMarket) external view returns (uint256) {
+
+    }
+
+    function getMarketBorrows(address lendingMarket) external view returns (uint256) {
+
+    }
+
+    function getMarketAssets(address lendingMarket) external view returns (address[] memory) {
+        
+    }
+
+    function getMarketCollateralAssets(address lendingMarket) external view returns (address[] memory) {
+        
+    }
+
+    function getMarketDebtAssets(address lendingMarket) external view returns (address[] memory) {
+        
+    }
+
+    function getTokenTVL(address token) external view returns (uint256) {
+
+    }
+
+    function getTokenBorrows(address token) external view returns (uint256) {
+
+    }
+
+    function getBaseRewards(address token) external view returns (uint256) {
+
+    }
+
+    function getCVERewards(address token) external view returns (uint256) {
+
+    }
+
     function getPrices(
         address[] calldata assets,
         bool[] calldata inUSD,
@@ -57,6 +106,8 @@ contract CurvanceAuxiliaryData {
         return _getCVELocker().hasRewardsToClaim(user);
     }
 
+    /// INTERNAL FUNCTIONS ///
+
     function _getCVELocker() internal view returns (ICVELocker) {
         return ICVELocker(centralRegistry.cveLocker());
     }
@@ -67,6 +118,16 @@ contract CurvanceAuxiliaryData {
 
     function _isLendingMarket(address market) internal view returns (bool) {
         return centralRegistry.isLendingMarket(market);
+    }
+
+    function _getTokenPrice(address mToken, bool getLower) internal view returns (uint256 price) {
+        uint256 errorCode;
+        (price, errorCode) = _getPriceRouter().getPrice(mToken, true, getLower);
+        // If we could not price the asset, bubble up a price of 0.
+        if  (errorCode == 2) {
+            price = 0;
+            return price;
+        }
     }
 
 }
