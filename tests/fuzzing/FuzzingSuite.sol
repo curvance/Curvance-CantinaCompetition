3243f71952c6950a8dbe43ecadc9522905e4d56a
Nat Chin <natalie.chin@trailofbits.com>
2024-01-17 09:46:58 -0500

    Added DToken fuzz tests

--- tests/fuzzing/FuzzingSuite.sol
+++ tests/fuzzing/FuzzingSuite.sol
@@ -6,9 +6,11 @@ import { FuzzVECVE } from "tests/fuzzing/FuzzVECVE.sol";
 import { FuzzLendtroller } from "tests/fuzzing/FuzzLendtroller.sol";
 import { FuzzLendtrollerRBAC } from "tests/fuzzing/FuzzLendtrollerRBAC.sol";
 import { FuzzLendtrollerStateChecks } from "tests/fuzzing/FuzzLendtrollerStateChecks.sol";
+import { FuzzDToken } from "tests/fuzzing/FuzzDToken.sol";
 
 //
 contract FuzzingSuite is
+    FuzzDToken,
     FuzzLendtroller,
     FuzzLendtrollerRBAC,
     FuzzLendtrollerStateChecks,
