317c5b65e2bcbc78b893b9474a60d46f0625a0d5
zeuscurvance <zeus@curvance.com>
2024-01-17 21:30:03 -0500

    add tests for WstETHAggregator, SavingsDaiAggregator, StakedFraxAggregator

--- contracts/oracles/adaptors/wrappedAggregators/SavingsDaiAggregator.sol
+++ contracts/oracles/adaptors/wrappedAggregators/SavingsDaiAggregator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import { BaseWrappedAggregator } from "contracts/oracles/adaptors/wrappedAggregators/BaseWrappedAggregator.sol";
 import { IPotLike } from "contracts/interfaces/external/maker/IPotLike.sol";
+import { ISavingsDai } from "contracts/interfaces/external/maker/ISavingsDai.sol";
 
 contract SavingsDaiAggregator is BaseWrappedAggregator {
     address public sDai;
@@ -30,6 +31,6 @@ contract SavingsDaiAggregator is BaseWrappedAggregator {
     function getWrappedAssetWeight() public view override returns (uint256) {
         // We divide by 1e9 since chi returns in 1e27 format,
         // so we need to offset by 1e9 to get to standard 1e18 format.
-        return IPotLike(sDai).chi() / 1e9;
+        return IPotLike(ISavingsDai(sDai).pot()).chi() / 1e9;
     }
 }
