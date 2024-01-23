79d155df8a029e363fd41a63251f2546d7b74972
Nat Chin <natalie.chin@trailofbits.com>
2024-01-23 10:32:56 -0500

    Removed market manager related functions from fuzz-vecve branch

--- tests/fuzzing/FuzzingSuite.sol
+++ tests/fuzzing/FuzzingSuite.sol
@@ -3,19 +3,8 @@ pragma solidity ^0.8.17;
 
 import { TestStatefulDeployments } from "tests/fuzzing/TestStatefulDeployments.sol";
 import { FuzzVECVE } from "tests/fuzzing/FuzzVECVE.sol";
-import { FuzzMarketManager } from "tests/fuzzing/FuzzMarketManager.sol";
-import { FuzzMarketManagerRBAC } from "tests/fuzzing/FuzzMarketManagerRBAC.sol";
-import { FuzzMarketManagerStateChecks } from "tests/fuzzing/FuzzMarketManagerStateChecks.sol";
-import { FuzzDToken } from "tests/fuzzing/FuzzDToken.sol";
 
 //
-contract FuzzingSuite is
-    FuzzDToken,
-    FuzzMarketManager,
-    FuzzMarketManagerRBAC,
-    FuzzMarketManagerStateChecks,
-    FuzzVECVE,
-    TestStatefulDeployments
-{
+contract FuzzingSuite is FuzzVECVE, TestStatefulDeployments {
 
 }
