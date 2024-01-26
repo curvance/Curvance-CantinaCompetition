289582e579bcdf903936b0d158fe1d554e17b4da
0xMai <chris@curvance.com>
2024-01-26 17:54:30 -0500

    Add ticker string parameter input in api3 oracle adaptor test

--- tests/oracles/OracleRouter/integrations/TestApi3Adaptor.t.sol
+++ tests/oracles/OracleRouter/integrations/TestApi3Adaptor.t.sol
@@ -14,6 +14,7 @@ contract TestApi3Adaptor is TestBaseOracleRouter {
         0x669bFFFAb8866d84F832abF90Dc9c1D73b7525Bc;
     address private CHAINLINK_PRICE_FEED_ETH =
         0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
+    string memory private ARB_TICKER = "ARB/USD";
 
     Api3Adaptor public adapter;
 
@@ -32,7 +33,7 @@ contract TestApi3Adaptor is TestBaseOracleRouter {
         centralRegistry.setOracleRouter(address(oracleRouter));
 
         adapter = new Api3Adaptor(ICentralRegistry(address(centralRegistry)));
-        adapter.addAsset(ARB, DAPI_PROXY_ARB_USD, 0, true);
+        adapter.addAsset(ARB, ARB_TICKER, DAPI_PROXY_ARB_USD, 0, true);
 
         oracleRouter.addApprovedAdaptor(address(chainlinkAdaptor));
 
