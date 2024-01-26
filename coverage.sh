0b96e8faf53413e0c598d5528429903cce3fd17e
Austinhs <austin_h_s@hotmail.com>
2024-01-26 11:26:39 -0500

    update coverage report to exclude mock + update report

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 forge coverage --report lcov
-lcov --remove lcov.info  -o lcov.info 'tests/*' 'script/*' '**/libraries/external/*' '**/testnet/*'
+lcov --remove lcov.info  -o lcov.info 'tests/*' 'script/*' '**/libraries/external/*' '**/testnet/*' 'mocks/*'
 genhtml lcov.info -o coverage_report
 google-chrome --headless --window-size=1200,800 --screenshot="coverage_report/coverage.png" "coverage/index.html"
 printf "<img src=\"coverage.png\"/>" > coverage_report/README.md
-## open coverage_report/index.html
+open coverage_report/index.html
