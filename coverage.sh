1333301502d746e4def5159fd90ed637fb364c61
Austinhs <austin_h_s@hotmail.com>
2024-01-25 23:03:02 -0500

    move from "report" folder to "coverage_report"

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 forge coverage --report lcov
 lcov --remove lcov.info  -o lcov.info 'tests/*' 'script/*' '**/libraries/external/*' '**/testnet/*'
-genhtml lcov.info -o report
-google-chrome --headless --screenshot="report/coverage.png" "report/index.html"
-printf "<img src="coverage.png"/>" > report/README.md
-open report/index.html
+genhtml lcov.info -o coverage_report
+google-chrome --headless --screenshot="coverage_report/coverage.png" "coverage/index.html"
+printf "<img src=\"coverage.png\"/>" > coverage_report/README.md
+## open coverage_report/index.html
