16c59be87c731a2a1371a76f85852a45372f2ff3
Austinhs <austin_h_s@hotmail.com>
2024-01-25 22:47:38 -0500

    add timestamping for report

--- coverage.sh
+++ coverage.sh
@@ -2,4 +2,5 @@
 forge coverage --report lcov
 lcov --remove lcov.info  -o lcov.info 'tests/*' 'script/*' '**/libraries/external/*' '**/testnet/*'
 genhtml lcov.info -o report
+printf "## Last updated: $(date +%F_%T)" > report/README.md
 open report/index.html
