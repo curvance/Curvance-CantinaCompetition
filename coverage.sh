fd02271aac0ba64ac5cc09eb9e04013562c9414f
Austinhs <austin_h_s@hotmail.com>
2024-01-25 23:08:15 -0500

    improve screenshot to get the entire test coverage screen

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,6 @@
 forge coverage --report lcov
 lcov --remove lcov.info  -o lcov.info 'tests/*' 'script/*' '**/libraries/external/*' '**/testnet/*'
 genhtml lcov.info -o coverage_report
-google-chrome --headless --screenshot="coverage_report/coverage.png" "coverage/index.html"
+google-chrome --headless --window-size=1200,800 --screenshot="coverage_report/coverage.png" "coverage/index.html"
 printf "<img src=\"coverage.png\"/>" > coverage_report/README.md
 ## open coverage_report/index.html
