f756f34d23d4e498872c09771928868dc7e8524d
0xMai <chris@curvance.com>
2024-02-17 02:15:23 -0500

    update IBlastNativeYieldManager

--- contracts/interfaces/blast/IBlastNativeYieldManager.sol
+++ contracts/interfaces/blast/IBlastNativeYieldManager.sol
@@ -2,6 +2,20 @@
 pragma solidity ^0.8.17;
 
 interface IBlastNativeYieldManager {
+    /// @notice Claims delegated yield on behalf of the caller. Will natively
+    ///         revert if not delegated. Transfers rewards to governee
+    /// @dev Only callable by governee themself. Natively claims gas rebate.
+    /// @param marketManager The Market Manager associated with the mToken
+    ///                      managing it native yield.
+    /// @param claimWETHYield Whether WETH native yield should be claimed.
+    /// @param claimUSDBYield Whether USDB native yield should be claimed.
+    /// @return WETHYield The amount of WETH yield claimed.
+    /// @return USDBYield The amount of USDB yield claimed.
+    function claimYieldForAutoCompounding(
+        address marketManager,
+        bool claimWETHYield,
+        bool claimUSDBYield
+    ) external returns (uint256, uint256);
     /// @notice Called by the Central registry on updating isMarketManager on Blast.
     /// @dev Only callable by Curvance Central Registry.
     /// @param notifiedMarketManager The Market Manager contract to modify support
